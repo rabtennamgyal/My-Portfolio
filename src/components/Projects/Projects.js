9480b3cc9bd22e03c2bffecb9e62ae09e137f022
Rabten <rabten@protonmail.com>
2021-07-27 00:06:46 -0400

    still on project.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,13 @@
 import img from '../../Assets/arrowDown.svg'
 
 function Projects() {
+
+    const one = document.getElementsByClassName('project one')
+
+    const log = () => {
+        console.log(one)
+    }
+
     return (
         <div className='Projects' id='projects'>
             <div className='head'>
@@ -11,13 +18,14 @@ function Projects() {
             </div>
             <div className='content'>
                 <div className='left arrow'>
-                    <p>
+                    <p onClick={log}>
                         &larr;
                     </p>
                 </div>
 
                 <div className='carousel'>
-                    
+
+                    {/* Project One*/}
                     <div className='project one'>
                         <div className='title'>
                             <h1>
@@ -54,9 +62,36 @@ function Projects() {
                         </div>
                     </div>
                     
-                    {/* <div className='project two'>
-                        two
-                    </div> */}
+                    {/* Project Two*/}
+                    <div className='project two'>
+                        <div className='title'>
+                            <h1>
+                                E-Commerce
+                            </h1>
+                            <p>
+                                An E-commerce site build using React.
+                            </p>
+                        </div>
+                        <div className='links'>
+                            <button>
+                                <a 
+                                href='https://e-com-fas.herokuapp.com/'
+                                target='_blank'
+                                >
+                                    See Project
+                                </a>
+                            </button>
+                            
+                            <button>
+                                <a 
+                                href='https://github.com/rabtennamgyal/e-com-fashion'
+                                target='_blank'
+                                >
+                                See Code
+                                </a>
+                            </button>
+                        </div>
+                    </div>
 
                 </div>
                 
