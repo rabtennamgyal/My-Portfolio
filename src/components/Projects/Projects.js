0ee94ed08e4b572a4aca401c6107e54741806396
Rabten <rabten@protonmail.com>
2021-07-26 22:55:02 -0400

    working on project slides

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,62 +10,61 @@ function Projects() {
                 </h1>
             </div>
             <div className='content'>
-                {/* <div className='right'>
-                    <p>
-                        &rarr;
-                    </p>
-                </div> */}
-
-                <div className='project one'>
-                    <div className='title'>
-                        <h1>
-                            Babylon 
-                        </h1>
-                        <p>
-                            An 
-                                <a href='https://www.acorns.com/' target='_blank' id='acorn'>
-                                Acorn
-                                </a> 
-                            inspired Brokerage type website showcasing my 
-                            Front-End Skills. Build using React and Sass; 
-                            fully responsive.
-                        </p>
-                    </div>
-                    <div className='links'>
-                        <button>
-                            <a 
-                            href='https://babylon123.herokuapp.com/'
-                            target='_blank'
-                            >
-                                See Project
-                            </a>
-                        </button>
-                        
-                        <button>
-                            <a 
-                            href='https://github.com/rabtennamgyal/babylon'
-                            target='_blank'
-                            >
-                            See Code
-                            </a>
-                        </button>
-                    </div>
-                </div>
-                {/* <div className='project two'>
-                    two
-                </div>
-                <div className='project three'>
-                    three
-                </div>
-                <div className='project four'>
-                    four
-                </div> */}
-                
-                {/* <div className='left'>
+                <div className='left arrow'>
                     <p>
                         &larr;
                     </p>
-                </div> */}
+                </div>
+
+                <div className='carousel'>
+                    
+                    <div className='project one'>
+                        <div className='title'>
+                            <h1>
+                                Babylon 
+                            </h1>
+                            <p>
+                                An 
+                                    <a href='https://www.acorns.com/' target='_blank' id='acorn'>
+                                    Acorn
+                                    </a> 
+                                inspired Brokerage type website showcasing my 
+                                Front-End Skills. Build using React and Sass; 
+                                fully responsive.
+                            </p>
+                        </div>
+                        <div className='links'>
+                            <button>
+                                <a 
+                                href='https://babylon123.herokuapp.com/'
+                                target='_blank'
+                                >
+                                    See Project
+                                </a>
+                            </button>
+                            
+                            <button>
+                                <a 
+                                href='https://github.com/rabtennamgyal/babylon'
+                                target='_blank'
+                                >
+                                See Code
+                                </a>
+                            </button>
+                        </div>
+                    </div>
+                    
+                    {/* <div className='project two'>
+                        two
+                    </div> */}
+
+                </div>
+                
+                <div className='right arrow'>
+                    <p>
+                        &rarr;
+                    </p>
+                </div>
             </div>
         </div>
     )
