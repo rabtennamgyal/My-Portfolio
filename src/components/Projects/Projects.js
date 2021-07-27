a1dd7e610a17fafc214a708f04e352a58ac00e3e
Rabten <rabten@protonmail.com>
2021-07-27 01:57:41 -0400

    new

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,7 @@
 import img from '../../Assets/arrowDown.svg'
+import Arr from './ProjectsArr'
 
 function Projects() {
-
-    const one = document.getElementsByClassName('project one')
-
-    const log = () => {
-        console.log(one)
-    }
 
     return (
         <div className='Projects' id='projects'>
@@ -18,29 +13,25 @@ function Projects() {
             </div>
             <div className='content'>
                 <div className='left arrow'>
-                    <p onClick={log}>
+                    <p>
                         &larr;
                     </p>
                 </div>
 
                 <div className='carousel'>
+                    <div className='project'>
 
-                    {/* Project One*/}
-                    <div className='project one'>
                         <div className='title'>
                             <h1>
-                                Babylon 
+                                Babylon
                             </h1>
                             <p>
-                                An 
-                                    <a href='https://www.acorns.com/' target='_blank' id='acorn'>
-                                    Acorn
-                                    </a> 
-                                inspired Brokerage type website showcasing my 
+                                An Acorn inspired Brokerage type website showcasing my 
                                 Front-End Skills. Build using React and Sass; 
                                 fully responsive.
                             </p>
                         </div>
+
                         <div className='links'>
                             <button>
                                 <a 
@@ -60,39 +51,8 @@ function Projects() {
                                 </a>
                             </button>
                         </div>
-                    </div>
-                    
-                    {/* Project Two*/}
-                    <div className='project two'>
-                        <div className='title'>
-                            <h1>
-                                E-Commerce
-                            </h1>
-                            <p>
-                                An E-commerce site build using React.
-                            </p>
-                        </div>
-                        <div className='links'>
-                            <button>
-                                <a 
-                                href='https://e-com-fas.herokuapp.com/'
-                                target='_blank'
-                                >
-                                    See Project
-                                </a>
-                            </button>
-                            
-                            <button>
-                                <a 
-                                href='https://github.com/rabtennamgyal/e-com-fashion'
-                                target='_blank'
-                                >
-                                See Code
-                                </a>
-                            </button>
-                        </div>
-                    </div>
 
+                    </div>
                 </div>
                 
                 <div className='right arrow'>
@@ -106,3 +66,45 @@ function Projects() {
 }
 
 export default Projects
+
+
+
+
+
+
+{/*}
+<div className='title'>
+<h1>
+    {Arr[0].name}
+</h1>
+<p>
+    An 
+        <a href='https://www.acorns.com/' target='_blank' id='acorn'>
+        Acorn
+        </a> 
+    inspired Brokerage type website showcasing my 
+    Front-End Skills. Build using React and Sass; 
+    fully responsive.
+</p>
+</div>
+
+<div className='links'>
+<button>
+    <a 
+    href='https://babylon123.herokuapp.com/'
+    target='_blank'
+    >
+        See Project
+    </a>
+</button>
+
+<button>
+    <a 
+    href='https://github.com/rabtennamgyal/babylon'
+    target='_blank'
+    >
+    See Code
+    </a>
+</button>
+</div>
+*/}
---
+++ src/components/Projects/ProjectsArr.js
@@ -0,0 +1,14 @@
+const Arr = [
+    {
+        name: 'Babylon',
+        link: 'https://babylon123.herokuapp.com/',
+        code: 'https://github.com/rabtennamgyal/babylon'
+    },
+    {
+        name: 'E-Commerce',
+        link: 'https://e-com-fas.herokuapp.com/',
+        code: 'https://github.com/rabtennamgyal/e-com-fashion'
+    }
+];
+
+export default Arr
