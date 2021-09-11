dbf0ef9d626d8025eac0becb0b0644e0804f82bb
Rabten <rabten@protonmail.com>
2021-09-11 06:10:56 -0400

    new project

--- src/components/Projects/ProjectsArr.js
+++ src/components/Projects/ProjectsArr.js
@@ -6,10 +6,10 @@ const Arr = [
         text: 'An Acorn inspired Brokerage type website showcasing my Front-End Skills. Build using React and Sass; fully responsive.'
     },
     {
-        name: 'E-Commerce',
-        link: 'https://e-com-fas.herokuapp.com/',
-        code: 'https://github.com/rabtennamgyal/e-com-fashion',
-        text: 'An E-Commerce Site build using React and Sass. Fully responsive but with no Back-End functionality yet.'
+        name: 'Insurer',
+        link: 'https://mighty-sierra-02192.herokuapp.com/',
+        code: 'https://github.com/rabtennamgyal/Insure-landing-FM',
+        text: 'A Front End Mentor Challenge build using React and Sass. Fully responsive but with no Back-End functionality yet.'
     },
     {
         name: 'Blog Landing Page',
