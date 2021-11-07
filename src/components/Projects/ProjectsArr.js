4caa7e0a33f8c1bcfedc6ea9a0d912039843c42b
Rabten <rabten@protonmail.com>
2021-11-07 01:54:54 -0400

    added new project data

--- src/components/Projects/ProjectsArr.js
+++ src/components/Projects/ProjectsArr.js
@@ -12,17 +12,17 @@ const Arr = [
         text: 'A Front End Mentor Challenge build using React and Sass. Fully responsive but with no Back-End functionality yet.'
     },
     {
-        name: 'Blog Landing Page',
-        link: 'https://blog-fem.herokuapp.com/',
-        code: 'https://github.com/rabtennamgyal/blog-landing-FM',
-        text: 'A Front End Mentor challenge build using React and Sass.'
-    },
-    {
         name: 'IP Address Tracker',
         link: 'https://ipppppppp.herokuapp.com/',
         code: 'https://github.com/rabtennamgyal/IP-address-FM',
         text: 'A Front End Mentor challenge with API.'
-    }
+    },
+    {
+        name: 'tic Tac Toe',
+        link: 'https://rabtennamgyal.github.io/TOP-Project-tictactoe/',
+        code: 'https://github.com/rabtennamgyal/TOP-Project-tictactoe',
+        text: 'Tic Tac Toe game build with Sass and Vanilla JavaScript. You can play with other players or an AI.'
+    },
 ];
 
 export default Arr
